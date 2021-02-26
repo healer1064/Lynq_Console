4030579c2a6146bd1a31af3ec9f33da108bdf9e0
Taimur Qazi <56827674+qazi662@users.noreply.github.com>
2021-02-26 19:28:05 +0500

    New Appointment

---
+++ components/NewAppointment/NewAppointmentModal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewAppointmentModal = ({ setModal }) => {
+    return (
+        <div className="new-appointment-modal" >
+            <div>
+                <img src="/img/new-appointment-close-modal.png" alt="close-modal" className="new-appointment-modal-close" onClick={() => setModal(false)} />
+                <img src="/img/new-appointment-success.png" alt="success" />
+                <p>Your message was sent to Ling’s team</p>
+                <button onClick={() => setModal(false)} >Confirm</button>
+            </div>
+        </div>
+    )
+}
+
+export default NewAppointmentModal
