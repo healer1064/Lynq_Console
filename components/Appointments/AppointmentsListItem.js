f45a316b2f3d8e2e6b2cbed93820d03ee9a25713
Taimur Qazi <56827674+qazi662@users.noreply.github.com>
2021-03-17 20:06:55 +0500

    appointment changes

--- components/Appointments/AppointmentsListItem.js
+++ components/Appointments/AppointmentsListItem.js
@@ -12,45 +12,49 @@ const AppointmentsListItem = ({ data }) => {
   const { day, date, appointments, status, userAppointments } = data;
 
   return (
-    <div
-      className={`appointments-col__card__wrp 
+    <Fade right cascade duration={1000}>
+      <div
+        className={`appointments-col__card__wrp 
       ${status === "inactive" && "inactive"}
       ${open && "active"}
       `}
-    >
-      <div
-        onClick={() => {
-          userAppointments && setOpen(!open);
-        }}
-        className="appointments-col__card"
       >
-        <div className="det">
-          {day}
-          <div className="line"></div>
-          {date}
-          <div className="line"></div>
-          <b>{appointments} appointments</b>
+        <div
+          onClick={() => {
+            userAppointments && setOpen(!open);
+          }}
+          className="appointments-col__card"
+        >
+          <div className="det">
+            {day}
+            <div className="line"></div>
+            {date}
+            <div className="line"></div>
+            <b>{appointments} appointments</b>
+          </div>
+          <div className="arrow">
+            <svg
+              width="14"
+              height="12"
+              viewBox="0 0 14 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M7 12L0.0717964 0L13.9282 0L7 12Z" fill="#7E88F4" />
+            </svg>
+          </div>
         </div>
-        <div className="arrow">
-          <svg
-            width="14"
-            height="12"
-            viewBox="0 0 14 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M7 12L0.0717964 0L13.9282 0L7 12Z" fill="#7E88F4" />
-          </svg>
-        </div>
+        {open && userAppointments && (
+          <div style={{ width: "100%" }}>
+            <Fade collapse duration={2000}>
+              {userAppointments.map((data, index) => (
+                <AppointmentCard key={index} data={data} />
+              ))}
+            </Fade>
+          </div>
+        )}
       </div>
-      {open &&
-        userAppointments &&
-        userAppointments.map((data, index) => (
-          <Fade key={index} duration={1000}>
-            <AppointmentCard data={data} />
-          </Fade>
-        ))}
-    </div>
+    </Fade>
   );
 };
 
