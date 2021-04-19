26e3b46c7a90ac2f5d6c4583bdb5d8f95fbe25b9
Taimur Qazi <56827674+qazi662@users.noreply.github.com>
2021-04-19 05:25:17 +0500

    Changes

--- components/Appointments/Request/RequestList.jsx
+++ components/Appointments/Request/RequestList.jsx
@@ -1,32 +1,15 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import Fade from "react-reveal/Fade";
 import moment from "moment";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useRouter } from "next/router";
 
 // style
 import styles from "./Request.module.css";
-import RequestDetail from "./RequestDetail";
 
-// context
-import ProfileContext from "../../../context/profile";
-
-const RequestList = ({ requestList, apt, success, setSuccess }) => {
-  // context
-  const { token } = useContext(ProfileContext);
-
+const RequestList = ({ requestList }) => {
   // router
   const router = useRouter();
-
-  // states
-  const [showDetail, setShowDetail] = useState(false);
-  const [requestData, setRequsetData] = useState(null);
-
-  const toggle = (_flag, _data) => {
-    setShowDetail(_flag);
-    setRequsetData(_data);
-  };
 
   return requestList.length === 0 ? (
     <div className="no-appointments">
@@ -34,61 +17,42 @@ const RequestList = ({ requestList, apt, success, setSuccess }) => {
     </div>
   ) : (
     <div className={styles.request__list}>
-      {!showDetail ? (
-        requestList.map((item, i) => (
-          <Fade key={i} duration={800} delay={50}>
-            <div
-              key={item}
-              onClick={() => router.push(`/appointments/requests/${item.id}`)}
-              className={`appointments-col__card__wrp`}
-            >
-              <div className="appointments-col__card">
-                <div className="det">
-                  <b>
-                    {moment(item.starting_date).format("ddd, MMMM DD, YYYY")}
-                  </b>
-                  <div className="line"></div>
-                  {item.activity_name}
-                  <div className="line"></div>
-                  {item.email}
-                  <div className="line"></div>
-                  {"Invitation Sent: "}
-                </div>
-                <div
-                  className="arrow"
-                  style={{ transform: "rotate(270deg)", marginLeft: "10px" }}
+      {requestList.map((item, i) => (
+        <Fade key={i} duration={800} delay={50}>
+          <div
+            key={item}
+            onClick={() => router.push(`/appointments/requests/${item.id}`)}
+            className={`appointments-col__card__wrp`}
+          >
+            <div className="appointments-col__card">
+              <div className="det">
+                <b>{moment(item.starting_date).format("ddd, MMMM DD, YYYY")}</b>
+                <div className="line"></div>
+                {item.activity_name}
+                <div className="line"></div>
+                {item.email}
+                <div className="line"></div>
+                {"Invitation Sent: "}
+              </div>
+              <div
+                className="arrow"
+                style={{ transform: "rotate(270deg)", marginLeft: "10px" }}
+              >
+                <svg
+                  width="14"
+                  height="12"
+                  viewBox="0 0 14 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    width="14"
-                    height="12"
-                    viewBox="0 0 14 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M7 12L0.0717964 0L13.9282 0L7 12Z"
-                      fill="#7E88F4"
-                    />
-                  </svg>
-                </div>
+                  <path d="M7 12L0.0717964 0L13.9282 0L7 12Z" fill="#7E88F4" />
+                </svg>
               </div>
             </div>
-          </Fade>
-        ))
-      ) : (
-        <>
-          <RequestDetail
-            data={requestData}
-            toggle={toggle}
-            apt={apt}
-            requestAccept={onAccept}
-            requestReject={onReject}
-            rejectLoading={rejectLoading}
-            acceptLoading={acceptLoading}
-            ToastContainer={ToastContainer}
-          />
-        </>
-      )}
+          </div>
+        </Fade>
+      ))}
+      )
     </div>
   );
 };
--- components/Settings/SettingsCallSync.jsx
+++ components/Settings/SettingsCallSync.jsx
@@ -1,5 +1,5 @@
 // libs
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import Fade from "react-reveal/Fade";
 
 // context
@@ -9,6 +9,71 @@ const SettingsCallSync = () => {
   // contect
   const { profile } = useContext(ProfileContext);
   const [isConnected, setIsConnected] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const toggleSuccess = () => {
+    setSuccess(!success);
+  };
+
+  useEffect(() => {
+    if (profile) {
+      fetchStatus();
+    }
+  }, [profile, success]);
+
+  const fetchStatus = async () => {
+    const config = {
+      method: "GET",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await fetch(
+        `https://cal.lynq.app/statud?uid=${profile.id}`,
+        config
+      );
+      const _data = await response.json();
+
+      setIsConnected(_data);
+    } catch (err) {
+      setIsConnected(null);
+      toast.error("Error, Failed to Fetch Request List!!!");
+    }
+  };
+
+  const disconnectCal = (id) => {
+    setLoading(true);
+    async function accept() {
+      const response = await fetch(
+        `https://cal.lynq.app/toggle?uid=${profile.id}`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return await response;
+    }
+
+    accept()
+      .then((res) => {
+        setLoading(false);
+        if (res.status === 200) {
+          toggleSuccess();
+        } else {
+          toast.error("Error, Failed To Disconnect Calendar");
+        }
+      })
+      .catch(() => {
+        toast.error("Error, Failed To Disconnect Calendar");
+        setLoading(false);
+      });
+  };
 
   return (
     <Fade duration={1000}>
@@ -17,9 +82,9 @@ const SettingsCallSync = () => {
         <div className="call-sync__calendar">
           <img src="/img/google-calendar.svg" alt="" />
           <a
-            href={`https://cal.lynq.app/?uid=${profile.id}`}
+            href={!isConnected && `https://cal.lynq.app/?uid=${profile.id}`}
             target="_blank"
-            onClick={() => setIsConnected(!isConnected)}
+            onClick={() => isConnected && disconnectCal}
             style={{
               textTransform: "lowercase",
               textDecoration: "none",
