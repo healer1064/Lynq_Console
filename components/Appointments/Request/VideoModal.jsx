2d77bbe7c83326ab2658e000c549a0727badcf8b
Taimur Qazi <56827674+qazi662@users.noreply.github.com>
2021-04-25 01:44:41 +0500

    Backoffice screens

---
+++ components/Appointments/Request/VideoModal.jsx
@@ -0,0 +1,16 @@
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const VideoModal = ({ setVideoModal, source }) => {
+  return (
+    <div className="video-modal" onClick={() => setVideoModal(false)}>
+      <AiFillCloseCircle color="white" onClick={() => setVideoModal(false)} />
+      <iframe
+        width="420"
+        height="315"
+        src={source.videoFileURL || "https://www.youtube.com/embed/tgbNymZ7vqY"}
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoModal;
