006cd198c06484110fb458fb539cf20cf535b500
ryuman <rioman19@gmail.com>
2021-07-25 21:54:07 +0100

    Bug on activate/deacivate ---toggle

--- components/Calls/Template/Content/index.jsx
+++ components/Calls/Template/Content/index.jsx
@@ -12,162 +12,202 @@ import ProfileContext from "@/context/profile";
 
 // requests
 import {
-  postOneToOneOptionReq,
-  putOneToOneOptionReq,
+	postOneToOneOptionReq,
+	putOneToOneOptionReq,
 } from "@/utils/requests/calls/template";
 
 // components
 import List from "../List";
 import Loading from "@/components/common/Loading";
 
+import { postProfileReq } from "@/utils/requests/public-profile";
+
 const index = ({ data }) => {
-  // context
-  const { token } = useContext(ProfileContext);
+	// context
+	const { token } = useContext(ProfileContext);
 
-  // states
-  const [active, setActive] = useState(false);
-  const [options, setOptions] = useState([
-    { id: 1, length: 15, status: false },
-    { id: 2, length: 30, status: false },
-    { id: 3, length: 60, status: false },
-  ]);
-  const [loading, setLoading] = useState(false);
+	// states
+	const [active, setActive] = useState(false);
+	const [options, setOptions] = useState([
+		{ id: 1, length: 15, status: false },
+		{ id: 2, length: 30, status: false },
+		{ id: 3, length: 60, status: false },
+	]);
+	const [loading, setLoading] = useState(false);
 
-  console.log(options);
-  console.log(data);
+	console.log(options);
+	console.log(data);
 
-  useEffect(() => {
-    if (data.length > 0) {
-      data.forEach((item) => {
-        if (item.duration == 15) {
-          const arr = options;
-          arr[0] = { ...item, status: item.isActive, length: item.duration };
-          setOptions(arr);
-        }
-        if (item.duration == 30) {
-          const arr = options;
-          arr[1] = { ...item, status: true, length: item.duration };
-          setOptions(arr);
-        }
-        if (item.duration == 60) {
-          const arr = options;
-          arr[2] = { ...item, status: true, length: item.duration };
-          setOptions(arr);
-        }
-      });
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data.length > 0) {
+			data.forEach((item) => {
+				if (item.duration == 15) {
+					const arr = options;
+					arr[0] = { ...item, status: item.isActive, length: item.duration };
+					setOptions(arr);
+				}
+				if (item.duration == 30) {
+					const arr = options;
+					arr[1] = { ...item, status: true, length: item.duration };
+					setOptions(arr);
+				}
+				if (item.duration == 60) {
+					const arr = options;
+					arr[2] = { ...item, status: true, length: item.duration };
+					setOptions(arr);
+				}
+			});
+		}
+	}, [data]);
 
-  useEffect(() => {
-    options.filter((item) => item.status == true).length > 0
-      ? setActive(true)
-      : setActive(false);
-  }, [options]);
+	useEffect(() => {
+		options.filter((item) => item.status == true).length > 0
+			? setActive(true)
+			: setActive(false);
+	}, [options]);
 
-  // on switch change
-  function onChange(checked) {
-    options.filter((item) => item.status == true).length > 0
-      ? setActive(checked)
-      : toast.info("Please set an option first.");
-  }
+	// on switch change
+	function onChange(checked) {
+		console.log("switch");
+		if (options.filter((item) => item.status == true).length > 0) {
+      setActive(checked);
 
-  // handle click
-  const handleClick = () => {
-    if (options.filter((item) => item.status == true).length === 0) {
-      toast.info("Please select an option first.");
-    } else {
-      var check = false;
-      options
-        .filter((item) => item.status == true)
-        .forEach((element) => {
-          if (
-            element.price &&
-            element.price != "" &&
-            element.description != ""
-          ) {
-            check = true;
-          } else {
-            check = false;
-          }
-        });
-      if (check) {
-        options.forEach((item) => {
-          setLoading(true);
-          // if (data.filter((d) => d.id == item.id).length > 0) {
-          //   putOneToOneOptionReq(token, item.id, {
-          //     id: item.id,
-          //     name: item.name,
-          //     teacherId: item.teacherId,
-          //     description: item.description,
-          //     duration: item.duration,
-          //     price: item.price,
-          //     isActive: item.status,
-          //     cancellation_policy: item.cancellation_policy,
-          //     material_needed: item.material_needed,
-          //   })
-          //     .then((res) => {
-          //       setLoading(false);
-          //       if (res.status == 200) {
-          //         toast.success("All put good");
-          //       } else {
-          //         toast.error("Failed to save template options.");
-          //       }
-          //     })
-          //     .catch(() => {
-          //       setLoading(false);
-          //       toast.error("Failed to save template options.");
-          //     });
-          // } else {
-          if (item.status) {
-            postOneToOneOptionReq(token, {
-              id: uuidv4(),
-              name: "string",
-              teacherId: "string",
-              description: item.description,
-              duration: item.length,
-              price: item.price,
-              cancellation_policy: "string",
-              material_needed: "string",
-            })
-              .then((res) => {
-                setLoading(false);
-                if (res.status == 200) {
-                  toast.success("All good");
-                } else {
-                  toast.error("Failed to save template options.");
-                }
-              })
-              .catch(() => {
-                setLoading(false);
-                toast.error("Failed to save template options.");
-              });
-          }
-          // }
-        });
-      } else {
-        toast.info("Please fill all fields.");
-      }
-    }
-  };
+			const message = fetch(
+				`https://api.lynq.app/account/public-profile/toggle-feature/message?t=${token}`,
+				{
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-type": "application/json",
+					},
+				}
+			);
+			const privateSession = fetch(
+				`https://api.lynq.app/account/public-profile/toggle-feature/private-session?t=${token}`,
+				{
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-type": "application/json",
+					},
+				}
+			);
+			const masterclass = fetch(
+				`https://api.lynq.app/account/public-profile/toggle-feature/masterclass?t=${token}`,
+				{
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-type": "application/json",
+					},
+				}
+			);
 
-  return (
-    <div className={styles.content}>
-      <div className={styles.switch_wrap}>
-        <Switch
-          checked={active}
-          onChange={onChange}
-          className={active ? styles.switch_on : styles.switch_off}
-        />
-        <span>{active ? "Activate" : "Deactivate"}</span>
-      </div>
-      <p>Select the options to show on your public profile</p>
-      <h3>Length</h3>
-      <List options={options} setOptions={setOptions} />
-      <button onClick={handleClick} className={styles.btn}>
-        {loading ? <Loading /> : "Save"}
-      </button>
-    </div>
-  );
+			Promise.allSettled([message, privateSession, masterclass]).then(() => {
+				toast.success(checked ? "Activated" : "Disactivated");
+			});
+		} else {
+			toast.info("Please set an option first.");
+		}
+	}
+
+	// handle click
+	const handleClick = () => {
+		if (options.filter((item) => item.status == true).length === 0) {
+			toast.info("Please select an option first.");
+		} else {
+			var check = false;
+			options
+				.filter((item) => item.status == true)
+				.forEach((element) => {
+					if (
+						element.price &&
+						element.price != "" &&
+						element.description != ""
+					) {
+						check = true;
+					} else {
+						check = false;
+					}
+				});
+			if (check) {
+				options.forEach((item) => {
+					setLoading(true);
+					// if (data.filter((d) => d.id == item.id).length > 0) {
+					//   putOneToOneOptionReq(token, item.id, {
+					//     id: item.id,
+					//     name: item.name,
+					//     teacherId: item.teacherId,
+					//     description: item.description,
+					//     duration: item.duration,
+					//     price: item.price,
+					//     isActive: item.status,
+					//     cancellation_policy: item.cancellation_policy,
+					//     material_needed: item.material_needed,
+					//   })
+					//     .then((res) => {
+					//       setLoading(false);
+					//       if (res.status == 200) {
+					//         toast.success("All put good");
+					//       } else {
+					//         toast.error("Failed to save template options.");
+					//       }
+					//     })
+					//     .catch(() => {
+					//       setLoading(false);
+					//       toast.error("Failed to save template options.");
+					//     });
+					// } else {
+					if (item.status) {
+						postOneToOneOptionReq(token, {
+							id: uuidv4(),
+							name: "string",
+							teacherId: "string",
+							description: item.description,
+							duration: item.length,
+							price: item.price,
+							cancellation_policy: "string",
+							material_needed: "string",
+						})
+							.then((res) => {
+								setLoading(false);
+								if (res.status == 200) {
+									toast.success("All good");
+								} else {
+									toast.error("Failed to save template options.");
+								}
+							})
+							.catch(() => {
+								setLoading(false);
+								toast.error("Failed to save template options.");
+							});
+					}
+					// }
+				});
+			} else {
+				toast.info("Please fill all fields.");
+			}
+		}
+	};
+
+	return (
+		<div className={styles.content}>
+			<div className={styles.switch_wrap}>
+				<Switch
+					checked={active}
+					onChange={onChange}
+					className={active ? styles.switch_on : styles.switch_off}
+				/>
+				<span>{active ? "Activate" : "Deactivate"}</span>
+			</div>
+			<p>Select the options to show on your public profile</p>
+			<h3>Length</h3>
+			<List options={options} setOptions={setOptions} />
+			<button onClick={handleClick} className={styles.btn}>
+				{loading ? <Loading /> : "Save"}
+			</button>
+		</div>
+	);
 };
 
 export default index;
