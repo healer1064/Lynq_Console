7dade2e8935107e36b6b7b35b52ffe7a43d6dc31
Taimur Qazi <56827674+qazi662@users.noreply.github.com>
2021-07-31 14:20:32 +0500

    switch loading

--- components/Calls/Template/Content/index.jsx
+++ components/Calls/Template/Content/index.jsx
@@ -1,5 +1,5 @@
 // libraries
-import { useState, useEffect, useContext, useMemo } from "react";
+import { useState, useEffect, useContext, useCallback } from "react";
 import { Switch } from "antd";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
@@ -11,206 +11,167 @@ import styles from "./styles.module.sass";
 import ProfileContext from "@/context/profile";
 
 // requests
-import {
-	postOneToOneOptionReq,
-	putOneToOneOptionReq,
-} from "@/utils/requests/calls/template";
+import { postOneToOneOptionReq } from "@/utils/requests/calls/template";
 
 // components
 import List from "../List";
 import Loading from "@/components/common/Loading";
 
-import { postProfileReq } from "@/utils/requests/public-profile";
-import { IoTrainOutline } from "react-icons/io5";
-import { useCallback } from "react";
+const index = ({ activePrivateSession }) => {
+  // context
+  const { token, slugData } = useContext(ProfileContext);
 
-const index = ({ data, activePrivateSession }) => {
-	// context
-	const { token, slugData } = useContext(ProfileContext);
+  // states
+  const [active, setActive] = useState(activePrivateSession);
+  const [options, setOptions] = useState([
+    { length: 15, status: false, tags: [] },
+    { length: 30, status: false, tags: [] },
+    { length: 60, status: false, tags: [] },
+  ]);
+  const [requestSent, setRequestSent] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	// states
-	const [active, setActive] = useState(activePrivateSession);
-	const [options, setOptions] = useState([
-		{ length: 15, status: false, tags: [] },
-		{ length: 30, status: false, tags: [] },
-		{ length: 60, status: false, tags: [] },
-	]);
-	useEffect(async () => {
-		if (token) {
-			const res = await fetch(
-				`https://api.lynq.app/account/event-type?t=${token}`
-			);
-			const bulletpoints = await res.json();
-			const sets = Object.values({
-				15: { duration: 15, status: false, tags: [] },
-				30: { duration: 30, status: false, tags: [] },
-				60: { duration: 60, status: false, tags: [] },
-				...bulletpoints.reduce((acc, c) => {
-					acc[c.duration] = c;
-					c["status"] = true;
-					return acc;
-				}, {}),
-			});
-			setOptions(sets);
-		}
-	}, []);
+  useEffect(async () => {
+    if (token) {
+      const res = await fetch(
+        `https://api.lynq.app/account/event-type?t=${token}`,
+      );
+      const bulletpoints = await res.json();
+      const sets = Object.values({
+        15: { duration: 15, status: false, tags: [] },
+        30: { duration: 30, status: false, tags: [] },
+        60: { duration: 60, status: false, tags: [] },
+        ...bulletpoints.reduce((acc, c) => {
+          acc[c.duration] = c;
+          c["status"] = true;
+          return acc;
+        }, {}),
+      });
+      setOptions(sets);
+    }
+  }, []);
 
-	const [requestSent, setRequestSent] = useState(false);
+  // on switch change
+  async function onChange(checked) {
+    setRequestSent(true);
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+    };
+    const message = await fetch(
+      `https://api.lynq.app/account/public-profile/toggle-feature/private-session?t=${token}`,
+      fetchOptions,
+    );
+    slugData.active_private_session = (
+      await message.json()
+    ).active_private_session;
 
-	// useEffect(() => {
-	// 	console.log(options);
-	// }, [options]);
-	const [loading, setLoading] = useState(false);
+    setRequestSent(false);
+    setActive(checked && slugData.active_private_session);
+    toast.success(checked ? "Activated" : "Disactivated");
+  }
 
-	/* useEffect(() => {
-		if (data.length > 0) {
-			data.forEach((item) => {
-				if (item.duration == 15) {
-					const arr = options;
-					arr[0] = { ...item, status: item.isActive, length: item.duration };
-					setOptions(arr);
-				}
-				if (item.duration == 30) {
-					const arr = options;
-					arr[1] = { ...item, status: true, length: item.duration };
-					setOptions(arr);
-				}
-				if (item.duration == 60) {
-					const arr = options;
-					arr[2] = { ...item, status: true, length: item.duration };
-					setOptions(arr);
-				}
-			});
-		}
-	}, [data]); */
+  // handle click
+  const handleClick = useCallback(() => {
+    if (options.filter((item) => item.status == true).length === 0) {
+      toast.info("Please select an option first.");
+    } else {
+      setLoading(true);
+      const toBeExecuted = options.filter((d) => d.price && d.price != "");
 
-	// useEffect(() => {
-	// 	options.filter((item) => item.status == true).length > 0
-	// 		? setActive(true)
-	// 		: setActive(false);
-	// }, [options]);
+      debugger;
+      const toBeDeleted = toBeExecuted.filter(
+        (d) => d.id && d.status === false,
+      );
+      const toBeUpdated = toBeExecuted.filter((d) => d.id && d.status === true);
+      const toBeRecreated = toBeExecuted.filter(
+        (d) => !d.id && d.status === true,
+      );
 
-	// on switch change
-	async function onChange(checked) {
-		setRequestSent(true);
-		const fetchOptions = {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-type": "application/json",
-			},
-		};
-		const message = await fetch(
-			`https://api.lynq.app/account/public-profile/toggle-feature/private-session?t=${token}`,
-			fetchOptions
-		);
-		slugData.active_private_session = (
-			await message.json()
-		).active_private_session;
+      toBeDeleted.forEach(async (d) => {
+        await fetch(
+          `https://api.lynq.app/account/event-type/${d.id}?t=${token}`,
+          {
+            method: "DELETE",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          },
+        );
+      });
 
-		setRequestSent(false);
-		setActive(checked && slugData.active_private_session);
-		toast.success(checked ? "Activated" : "Disactivated");
-	}
+      toBeUpdated.forEach(async (d) => {
+        await fetch(
+          `https://api.lynq.app/account/event-type/${d.id}?t=${token}`,
+          {
+            method: "PUT",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(d),
+          },
+        );
+        toast.success("Updated succesfully");
+        toBeRecreated.length === 0 && setLoading(false);
+      });
 
-	// handle click
-	const handleClick = useCallback(() => {
-		if (options.filter((item) => item.status == true).length === 0) {
-			toast.info("Please select an option first.");
-		} else {
-			setLoading(true);
-			const toBeExecuted = options.filter((d) => d.price && d.price != "");
+      toBeRecreated.forEach((d) => {
+        const data = {
+          id: uuidv4(),
+          name: "string",
+          teacherId: "string",
+          description: d.description,
+          tags: d.tags,
+          duration: d.duration,
+          price: d.price,
+          cancellation_policy: "string",
+          material_needed: "string",
+        };
 
-			debugger;
-			const toBeDeleted = toBeExecuted.filter(
-				(d) => d.id && d.status === false
-			);
-			const toBeUpdated = toBeExecuted.filter((d) => d.id && d.status === true);
-			const toBeRecreated = toBeExecuted.filter(
-				(d) => !d.id && d.status === true
-			);
+        postOneToOneOptionReq(token, data)
+          .then((res) => {
+            d.id = data.id;
+            setLoading(false);
+            if (res.status == 200) {
+              toast.success("All good");
+            } else {
+              toast.error("Failed to save template options.");
+            }
+          })
+          .catch(() => {
+            setLoading(false);
+            toast.error("Failed to save template options.");
+          });
+      });
 
-			toBeDeleted.forEach(async (d) => {
-				await fetch(
-					`https://api.lynq.app/account/event-type/${d.id}?t=${token}`,
-					{
-						method: "DELETE",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-						},
-					}
-				);
-			});
+      !toBeExecuted.length && toast.info("Please fill all fields.");
+    }
+  }, [token, options]);
 
-			toBeUpdated.forEach(async (d) => {
-				await fetch(
-					`https://api.lynq.app/account/event-type/${d.id}?t=${token}`,
-					{
-						method: "PUT",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-						},
-						body: JSON.stringify(d),
-					}
-				);
-				toast.success("Updated succesfully");
-				toBeRecreated.length === 0 && setLoading(false);
-			});
-
-			toBeRecreated.forEach((d) => {
-				const data = {
-					id: uuidv4(),
-					name: "string",
-					teacherId: "string",
-					description: d.description,
-					tags: d.tags,
-					duration: d.duration,
-					price: d.price,
-					cancellation_policy: "string",
-					material_needed: "string",
-				};
-
-				postOneToOneOptionReq(token, data)
-					.then((res) => {
-						d.id = data.id;
-						setLoading(false);
-						if (res.status == 200) {
-							toast.success("All good");
-						} else {
-							toast.error("Failed to save template options.");
-						}
-					})
-					.catch(() => {
-						setLoading(false);
-						toast.error("Failed to save template options.");
-					});
-			});
-
-			!toBeExecuted.length && toast.info("Please fill all fields.");
-		}
-	}, [token, options]);
-
-	return (
-		<div className={styles.content}>
-			<div className={styles.switch_wrap}>
-				<Switch
-					checked={active}
-					onChange={onChange}
-					loading={requestSent}
-					className={active ? styles.switch_on : styles.switch_off}
-				/>
-				<span>{active ? "Activate" : "Deactivate"}</span>
-			</div>
-			<p>Select the options to show on your public profile</p>
-			<h3>Length</h3>
-			<List options={options} setOptions={setOptions} />
-			<button onClick={handleClick} className={styles.btn}>
-				{loading ? <Loading /> : "Save"}
-			</button>
-		</div>
-	);
+  return (
+    <div className={styles.content}>
+      <div className={styles.switch_wrap}>
+        <Switch
+          checked={active}
+          onChange={onChange}
+          loading={requestSent}
+          className={active ? styles.switch_on : styles.switch_off}
+        />
+        <span>{active ? "Activate" : "Deactivate"}</span>
+      </div>
+      <p>Select the options to show on your public profile</p>
+      <h3>Length</h3>
+      <List options={options} setOptions={setOptions} />
+      <button onClick={handleClick} className={styles.btn}>
+        {loading ? <Loading /> : "Save"}
+      </button>
+    </div>
+  );
 };
 
 export default index;
