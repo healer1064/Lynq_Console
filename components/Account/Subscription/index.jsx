b28ce54f6fd99638875288d8cc478cbea965ab7c
Arun Jha <andjhait@gmail.com>
2021-11-03 20:37:46 +0530

    DAS-67: Fixed the cancel button issues

--- components/Account/Subscription/index.jsx
+++ components/Account/Subscription/index.jsx
@@ -9,7 +9,7 @@ const index = () => {
     <div className={styles.subscription}>
       <h3>Subscription</h3>
       <h6>Your Current Plan</h6>
-      <p>EARLY BIRD with 15% commission added on top of your price</p>
+      <p>15% commission added on top of your price</p>
       <h6>Processing Fees On Payments</h6>
       <p>2.9%</p>
     </div>
